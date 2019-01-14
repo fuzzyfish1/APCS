f85f3af519f60cefcc917836c2bd12cae296d219
occidentalis <occidentalis920@gmail.com>
2019-01-14 13:25:57 -0600

    Exercise 9.9 (Inheritance Practice)

---
+++ chapter9/Manager.java
@@ -0,0 +1,24 @@
+package chapter9;
+
+public class Manager extends Employee {
+
+    private String department;
+
+    public Manager(String name, double salary, String department) {
+        super(name, salary);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Department: " + department;
+    }
+}
--- sandbox/Sandbox.java
+++ sandbox/Sandbox.java
@@ -1,14 +1,19 @@
 package sandbox;
 
-import chapter9.Quadrilateral;
-import chapter9.Trapezoid;
+import chapter9.Employee;
+import chapter9.Executive;
+import chapter9.Manager;
 
 public class Sandbox {
 
     public static void main(String args[]) {
-        Object test = "Test?";
+        Employee john = new Employee("John Smith", 40320.0);
+        Employee jill = new Manager("Jill Ryker", 84560.0, "Accounting");
+        Employee max = new Executive("Max Tough", 122430.0, "Marketing");
 
-        System.out.println(test);
+        System.out.println(john);
+        System.out.println(jill);
+        System.out.println(max);
     }
 
 }
